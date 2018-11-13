13422fc59ef4aa6e2c54f391a31e499c75bb6c3e
root <root@LAPTOP-FRL2UG56.localdomain>
2018-11-13 16:48:31 +0100

    Add erosion w. kernel function, writer, and output

--- Erosion Experiment.py
+++ Erosion Experiment.py
@@ -7,18 +7,22 @@ Created on Tue Nov 13 10:41:20 2018
 
 import numpy as np
 from scipy import ndimage
+from writers import asciiOut 
 
 #Manually deleted first 6 text lines
 raster = np.loadtxt("raster.asc")
 
+###############################################################################
+# Specify main parameters:
+
 #Slope tolerance delta H max - tolerated increase in H for distance between two points
 dhmax = 0.3
-
 #Grid size
 a = 0.5
-
 #Kernel Size   
-kernel = (3,3) #only uneven numbers!
+kernel = (9,9) #only uneven numbers!
+
+###############################################################################
 
 def maxSlopeErosion(input, kernel, dhmax):
     def setupStructureElement(kernel, dhmax):
@@ -43,15 +47,21 @@ def maxSlopeErosion(input, kernel, dhmax):
     output = ndimage.morphology.grey_erosion(input, structure = s)
     return output
 
-def maxSlopeFilter(input, kernel, dhmax):
+def maxSlopeFilter(input_raster, kernel, dhmax):
     #Replace -9999 with NaN
-    input[input==-9999]=np.nan
+    input_raster[input_raster==-9999]=np.nan
+
     #Call Erosion
-    eroded = maxSlopeErosion(input, kernel, dhmax)
+    eroded = maxSlopeErosion(input_raster, kernel, dhmax)
+
     #Only return those below cutoff
-    output = np.where(input <= eroded, input, np.nan)
+    output = np.where(input_raster <= eroded, input_raster, np.nan)
     return output
 
+###############################################################################
+
+# Output Raster
 filtered = maxSlopeFilter(raster, kernel, dhmax)
-np.nanmin(filtered)
-        
+
+# Out to Ascii  
+asciiOut(filtered, "Slope_Erosion_filtered_{}kernel_{}dhmax".format(str(kernel[0])+str(kernel[1]), dhmax))
